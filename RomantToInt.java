7bb12af1ddacdb3dc3a19ba5737f1f30ff1e10ce
cuong <taihuu299@gmail.com>
2025-09-25 14:35:36 +0700

    binarySearch code added

---
+++ RomantToInt.java
@@ -0,0 +1,34 @@
+public class RomantToInt {
+  public static int getValue(char c) {
+    switch (c) {
+      case 'I': return 1;
+      case 'V': return 5;
+      case 'X': return 10;
+      case 'L': return 50;
+      case 'C': return 100;
+      case 'D': return 500;
+      case 'M': return 1000;
+      default: return 0;
+    }
+  }
+
+  public static int romanToInt(String s) {
+    int result = 0;
+    int prevValue = 0;
+    for (int i = s.length() - 1; i >= 0; i--) {
+      int value = getValue(s.charAt(i));
+      if (value < prevValue) {
+        result -= value;
+      } else {
+        result += value;
+      }
+    }
+    return result;
+  }
+  
+  public static void main(String[] args) {
+    String s = "MCMXCIV";
+    int result = romanToInt(s);
+    System.out.println(result);
+  }
+}
