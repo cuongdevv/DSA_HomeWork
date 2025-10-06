9c4d57df3a5a2ac48829f23e832f641189f16b49
cuongdevv <taihuu299@gmail.com>
2025-10-06 10:27:15 +0700

    wtf

--- LinkedList/DemoDSLK.java
+++ LinkedList/DemoDSLK.java
@@ -1,29 +1,29 @@
 package LinkedList;
 
 public class DemoDSLK {
-    public static void main(String[] args) {
-        MyDSLK myDS = new MyDSLK();
-        // head = null
-        // tail = null
-        // size = 0
+		public static void main(String[] args) {
+				MyDSLK myDS = new MyDSLK();
+				// head = null
+				// tail = null
+				// size = 0
 
-        Node node12 = new Node(12);
-        Node node99 = new Node(99);
-        Node node37 = new Node(37);
+				Node node12 = new Node(12);
+				Node node99 = new Node(99);
+				Node node37 = new Node(37);
 
-        node12.next = node99;
-        node99.next = node37;
-        node37.next = null;
-        // nút đầu của DSLK
-        myDS.head = node12;
-        myDS.tail = node37;
-        myDS.size = 3;
+				node12.next = node99;
+				node99.next = node37;
+				node37.next = null;
+				// nút đầu của DSLK
+				myDS.head = node12;
+				myDS.tail = node37;
+				myDS.size = 3;
 
-        // In danh sách liên kết
-        myDS.printDSLK();
-        // myDS.removeFirst();
-        myDS.printDSLK();
-        System.out.println(myDS.getFirst());
-        System.out.println(myDS.getLast());
-    }
+				// In danh sách liên kết
+				myDS.printDSLK();
+				// myDS.removeFirst();
+				myDS.printDSLK();
+				System.out.println(myDS.getFirst());
+				System.out.println(myDS.getLast());
+		}
 }
--- LinkedList/MyDSLK.java
+++ LinkedList/MyDSLK.java
@@ -1,54 +1,54 @@
 package LinkedList;
 
 public class MyDSLK {
-    Node head;
-    Node tail;
-    int size;
+		Node head;
+		Node tail;
+		int size;
 
-    MyDSLK() {
-        head = null;
-        tail = null;
-        size = 0;
-    }
+		MyDSLK() {
+				head = null;
+				tail = null;
+				size = 0;
+		}
 
-    void printDSLK() {
-        Node current = head;
-        while (current != null) {
-            System.out.print(current.data + " -> ");
-            current = current.next;
-        }
-        System.out.println("null");
-    }
+		void printDSLK() { // O(n)
+				Node current = head;
+				while (current != null) {
+						System.out.print(current.data + " -> ");
+						current = current.next;
+				}
+				System.out.println("null");
+		}
 
-    void insertFirst(int data) {
-        Node newNode = new Node(data);
-        newNode.next = head;
-        head = newNode;
-        size++;
-    }
+		void insertFirst(int data) { // O(1)
+				Node newNode = new Node(data);
+				newNode.next = head;
+				head = newNode;
+				size++;
+		}
 
-    void insertLast(int data) {
-        Node newNode = new Node(data);
-        tail.next = newNode;
-        tail = newNode;
-        size++;
-    }
+		void insertLast(int data) {
+				Node newNode = new Node(data);
+				tail.next = newNode;
+				tail = newNode;
+				size++;
+		}
 
-    void removeFirst() {
-        head = head.next;
-        size--;
-    }
+		void removeFirst() { // O(1)
+				head = head.next;
+				size--;
+		}
 
-    void removeLast() {
-        tail = tail.next;
-        size--;
-    }
+		void removeLast() { // O(1)
+				tail = tail.next;
+				size--;
+		}
 
-    int getFirst() {
-        return head.data;
-    }
+		int getFirst() { // O(1)
+				return head.data;
+		}
 
-    int getLast() {
-        return tail.data;
-    }
+		int getLast() { // O(1)
+				return tail.data;
+		}
 }
